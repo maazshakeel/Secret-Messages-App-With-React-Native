3cb8c36cf952f8bc4efd7d1e1f113bcc2ee847a0
maazshakeel <maaz.shakeel.bodla@gmail.com>
2021-06-16 02:10:05 +0500

    Experimenting with button styling

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, TextInput, Text, View } from 'react-native';
+import { StyleSheet, Pressable, Button, TextInput, Text, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textHeading}>Secret Messages</Text>
-        <View style={{flexDirection: 'row'}}>
-          <TextInput placeholderTextColor = "white" style={styles.input} placeholder="Message" />
-          <TextInput placeholderTextColor = "white" style={[styles.input, styles.keyInputStyling]} placeholder="Key" />
+        <View style={{flexDirection: 'row', marginTop: 50, marginBottom: 15}}>
+          <TextInput placeholderTextColor = "white" style={[styles.input, styles.messageInputStyling]} placeholder="Message" />
+          <TextInput keyboardType="numeric" placeholderTextColor = "white" style={[styles.input, styles.keyInputStyling]} placeholder="Key" />
         </View>
+        {/* <Button style={styles.button} title="Encrypt" color="#f194ff" /> */}
+        {/* <Pressable style={styles.button} >
+          <Text style={styles.text}>Save</Text>
+        </Pressable> */}
       </View>
     );
   }
@@ -25,21 +34,55 @@ const styles = StyleSheet.create({
   },
   textHeading: {
     fontSize: 48,
-    color: 'white'
+    color: 'white',
+    marginLeft: 12,
+    fontFamily: 'sans-serif-medium'
   },
   input: {
     borderWidth: 1,
     borderColor: 'white',
-    minWidth: 100,
     marginTop: 20,
     marginHorizontal: 20,
     paddingHorizontal: 10,
     paddingVertical: 18,
-    fontFamily: 'sans-serif',
-    fontSize: 18,
+    fontFamily: 'Roboto',
+    fontSize: 20, 
     borderRadius: 25,
   },
   keyInputStyling: {
     width: 15,
-  }
+    minWidth: 80,
+  },
+  messageInputStyling: {
+    width: 35,
+    minWidth: 262,
+    marginLeft: 7,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  encryptButton: {
+  }  
 });
