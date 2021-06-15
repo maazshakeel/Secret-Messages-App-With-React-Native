ae90e5465bbc938ed431eb0644c9c66f3246ab0e
maazshakeel <maaz.shakeel.bodla@gmail.com>
2021-06-16 01:36:52 +0500

    Just designed some inputs

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <Text style={styles.textHeading}>Secret Messages</Text>
+        <TextInput placeholderTextColor = "white" style={styles.input} placeholder="Message" />
+        <TextInput placeholderTextColor = "white" style={styles.input} placeholder="Key" />
       </View>
     );
   }
@@ -16,8 +18,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#232F34',
+    paddingTop: 50,
+  },
+  textHeading: {
+    fontSize: 48,
+    color: 'white'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'white',
+    minWidth: 100,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 18,
+    fontFamily: 'sans-serif',
+    fontSize: 18,
+    borderRadius: 25,
   },
 });
